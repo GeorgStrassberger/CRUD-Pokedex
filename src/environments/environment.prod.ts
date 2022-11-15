e3f2dce7e494956b1542bb99cf69d97b767df56f
Georg <georg.strassberger@googlemail.com>
2022-11-15 17:51:42 +0100

    add firebase & material

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'pokedex-e4e63',
+    appId: '1:157227692383:web:b4fefd52bb21a6d20186ea',
+    storageBucket: 'pokedex-e4e63.appspot.com',
+    apiKey: 'AIzaSyDHjj8R2XN_reINbxBcrkIFRUHsDEHKtGI',
+    authDomain: 'pokedex-e4e63.firebaseapp.com',
+    messagingSenderId: '157227692383',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'pokedex-e4e63',
+    appId: '1:157227692383:web:b4fefd52bb21a6d20186ea',
+    storageBucket: 'pokedex-e4e63.appspot.com',
+    apiKey: 'AIzaSyDHjj8R2XN_reINbxBcrkIFRUHsDEHKtGI',
+    authDomain: 'pokedex-e4e63.firebaseapp.com',
+    messagingSenderId: '157227692383',
+  },
   production: false
 };
 
